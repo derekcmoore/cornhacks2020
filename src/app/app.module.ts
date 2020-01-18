2009797a944ffe7a8395e6bd9de307061a687596
dmoore28 <moorder1112@gmail.com>
2020-01-18 12:28:50 -0600

    Created main questionnaire component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionnaireComponent } from './pages/questionnaire/questionnaire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionnaireComponent
   ],
   imports: [
     BrowserModule,
