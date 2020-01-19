00617954717d4345dfca321ae5f7a5e715d1ccdb
dmoore28 <moorder1112@gmail.com>
2020-01-19 07:58:20 -0600

    Added functions to calculate energy usage

--- src/app/pages/survey-results/survey-results.component.ts
+++ src/app/pages/survey-results/survey-results.component.ts
@@ -8,14 +8,35 @@ import { QuestionsService } from 'src/app/services/questions/questions.service';
 })
 export class SurveyResultsComponent implements OnInit {
 
+  runningComputers: number = 0;
+  size: number = 0;
   constructor(private questionsService: QuestionsService) { }
-
   ngOnInit() {
+    this.getSize();
   }
 
-  calculateCO2Emissions() {
+  getSize() {
+    if (this.questionsService.answers['q8'] == "small") {
+      this.size = 75;
+    }
+    else if (this.questionsService.answers['q8'] == "small") {
+      this.size = 500;
+    }
+    else {
+      this.size = 1000;
+    }
+  }
+
+  calculateCO2Emissions(hours: number) {
     let kwhToCO2 = 0.000707; //AVERT, U.S. national weighted average CO2 marginal emission rate, year 2018 data
-    return this.questionsService.answers['q8'] * kwhToCO2;
+    let months = 12;
+    let tonsToPounds = 2204.62;
+    return this.questionsService.answers['q8'] * kwhToCO2 * months * tonsToPounds;
+  }
+
+  calculateComputerEnergy() {
+    let averageComputerConsumtion = 125;
+    this.runningComputers = averageComputerConsumtion * this.size;
   }
 
 }
