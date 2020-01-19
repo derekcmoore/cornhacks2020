d53b706802ed4d23831f7cf6102c6853c8291f5e
Taylor <taylorbernt@gmail.com>
2020-01-19 07:07:34 -0600

    uisehfulisehbfuyliubseliubesg

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -8,17 +8,26 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 })
 export class SearchComponent implements OnInit {
 
-  urll = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyDD1xdeorc23ZIQWrapk8OF-XMo-aBmfR8&cx=007751619407156972941:todaeusxax4&q=environmentallyfriendly';
+  urll = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyDD1xdeorc23ZIQWrapk8OF-XMo-aBmfR8&cx=007751619407156972941:todaeusxax4&q=';
 
   httpOptions = {
     headers: new HttpHeaders({ "Content-Type": "application/json", "Authorization": "c31z" })
   };
 
+  items = null;
+
+  searchText = '';
+
   constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
-    this.http.get(this.urll, this.httpOptions).subscribe(x => {
+    this.showResults();
+  }
+
+  showResults(){
+    this.http.get(this.urll+"economically+friendly+"+this.searchText.replace(' ', '+')).subscribe(x => {
+      this.items = x['items'];
     });
   }
 
