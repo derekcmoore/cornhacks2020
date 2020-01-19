be8f0c69e695d96012bfb839b557642046bf098a
dmoore28 <moorder1112@gmail.com>
2020-01-18 23:55:31 -0600

    Changed question file locations to better align with our new mvp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,16 @@ import { QuestionOneBComponent } from './components/businesses/question-one-b/qu
 import { QuestionsComponent } from './components/questions/questions.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Question1IComponent } from './components/individual/question1-i/question1-i.component';
-import { Question2IComponent } from './components/individual/question2-i/question2-i.component';
-import { Question3IComponent } from './components/individual/question3-i/question3-i.component';
-import { Question4IComponent } from './components/individual/question4-i/question4-i.component';
-import { Question5IComponent } from './components/individual/question5-i/question5-i.component';
-import { Question6IComponent } from './components/individual/question6-i/question6-i.component';
-import { Question7IComponent } from './components/individual/question7-i/question7-i.component';
-import { Question8IComponent } from './components/individual/question8-i/question8-i.component';
-import { Question9IComponent } from './components/individual/question9-i/question9-i.component';
-import { Question10IComponent } from './components/individual/question10-i/question10-i.component';
+import { Question1IComponent } from './components/questions/question1-i/question1-i.component';
+import { Question2IComponent } from './components/questions/question2-i/question2-i.component';
+import { Question3IComponent } from './components/questions/question3-i/question3-i.component';
+import { Question4IComponent } from './components/questions/question4-i/question4-i.component';
+import { Question5IComponent } from './components/questions/question5-i/question5-i.component';
+import { Question6IComponent } from './components/questions/question6-i/question6-i.component';
+import { Question7IComponent } from './components/questions/question7-i/question7-i.component';
+import { Question8IComponent } from './components/questions/question8-i/question8-i.component';
+import { Question9IComponent } from './components/questions/question9-i/question9-i.component';
+import { Question10IComponent } from './components/questions/question10-i/question10-i.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
